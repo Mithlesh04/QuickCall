3f6f042169285a7b475f2c4c675a8a7363ffcb61
Mithlesh04 <Mithlesh04>
2024-01-14 19:35:38 +0530

    0.0.4

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,5 +1,5 @@
 import { Client } from "./client";
 import { Server } from "./server";
-declare const QcVersion = "0.0.3";
+declare const QcVersion = "0.0.4";
 export { Client, Server, QcVersion };
 export default Client;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { Client } from "./client"
 import { Server } from "./server"
 
-const QcVersion = "0.0.3"
+const QcVersion = "0.0.4"
 
 export {
     Client,
